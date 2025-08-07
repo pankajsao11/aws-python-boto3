f621d8438ab69f27d539798bfe24c09121d441e7
Pankaj Sahu <81400644+pankajsao11@users.noreply.github.com>
2025-08-07 18:42:07 +0530

    Create ec2.py

---
+++ ec2.py
@@ -0,0 +1,11 @@
+import boto3
+
+client = boto3.client('ec2')
+resp = client.run_instances(
+    ImageId='ami-084568db4383264d4',
+    InstanceType='t2.micro',
+    MinCount=MaxCount=1
+)
+
+for instance in resp["Instances"]:
+    print(f'The instance launched using boto is {instance}['InstanceId']')
