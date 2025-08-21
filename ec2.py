45a86044e455cc10155b62c482f7e0f6006faefe
Pankaj Sahu <81400644+pankajsao11@users.noreply.github.com>
2025-08-21 12:52:47 +0530

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -4,8 +4,9 @@ client = boto3.client('ec2')
 resp = client.run_instances(
     ImageId='ami-084568db4383264d4',
     InstanceType='t2.micro',
-    MinCount=MaxCount=1
+    MinCount=1,
+    MaxCount=1
 )
 
-for instance in resp["Instances"]:
-    print(f'The instance launched using boto is {instance}['InstanceId']')
+for instance in resp['Instances']:
+    print(f'The instance launched using boto is {instance['InstanceId']}')
