4fea9da5406320dcf6d36c39934dd04beeb5eec0
Pankaj Sahu <81400644+pankajsao11@users.noreply.github.com>
2025-08-21 13:17:10 +0530

    Create 2-ec2Operations.py

---
+++ 2-ec2Operations.py
@@ -0,0 +1,17 @@
+import boto3
+
+ec2 = boto3.client('ec2')\
+#for starting instances
+ec2.start_instances(InstanceIds=['i-02b3d6a0030115666', 'i-04e3a8eb0bca13045', 'i-0c565568f63b1672d']) 
+
+#for stopping instances
+ec2.stop_instances(InstanceIds=['i-02b3d6a0030115666', 'i-04e3a8eb0bca13045', 'i-0c565568f63b1672d']) 
+
+#for terminating instances:
+import boto3
+
+ec2 = boto3.client('ec2')
+resp = ec2.terminate_instances(InstanceIds=['i-02b3d6a0030115666', 'i-04e3a8eb0bca13045', 'i-0c565568f63b1672d'])
+
+for instance in resp['TerminatingInstances']:
+    print(f"The instance with ids {instance['InstanceId']} is terminated")
